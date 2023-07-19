a4824e5fd54164f4bd724c2741527fdf3650b612
Andrew Dao <daoanhphuc84@gmail.com>
2023-07-19 23:06:14 +0700

    [c] chore: config tailwind - config font - config color with "github" prefix

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import React from 'react';
+import './index.css';
 
 function App() {
     return (
         <>
-            <>
-                <div className="bg-white dark:bg-gray-800 ">
-                    <div className="lg:flex lg:items-center lg:justify-between w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
-                        <h2 className="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
-                            <span className="block">Want to be millionaire ?</span>
-                            <span className="block text-indigo-500">It&#x27;s today or never.</span>
-                        </h2>
-                        <div className="lg:mt-0 lg:flex-shrink-0">
-                            <div className=" inline-flex rounded-md shadow">
-                                <button
-                                    type="button"
-                                    className="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                                >
-                                    Get started
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </>
+            <div className="font-space">
+                Adipisicing eiusmod exercitation aliqua do ea id proident nostrud amet id
+                adipisicing. Sint est deserunt enim ex minim ad minim ex incididunt ex aliqua velit
+                sunt. Commodo laboris exercitation culpa dolor amet excepteur elit est sunt nulla
+                occaecat. Ad Lorem proident irure sint ad irure commodo voluptate in anim do.
+                Laborum dolore aliqua eu deserunt culpa labore ullamco sunt deserunt. Nulla enim
+                consectetur irure officia labore ipsum voluptate non consectetur. Nostrud nisi
+                cupidatat aliquip anim fugiat esse eu nulla deserunt consectetur.
+            </div>
+
+            <div className="font-space font-bold bg-github-gunMetal text-github-white text-[1.5rem]">
+                Ad cupidatat consectetur ullamco consectetur Lorem fugiat do sunt enim culpa minim
+                velit mollit. Laborum officia in excepteur aute nulla duis reprehenderit cupid
+                officia aliqua quis deserunt culpa. Anim consequat non cupidatat reprehenderit
+                aliqua. Labore id aute cupidatat incididunt. Reprehenderit proident quis eiusmod
+                incididunt cillum ex do labore ullamco occaecat aliquip nisi tempor nulla. Mollit
+                quis aliquip voluptate est non laborum Lorem magna sit adipisicing in ut.
+            </div>
         </>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                space: 'Space Mono',
+            },
+        },
+
+        colors: {
+            'github-azure': '#0079FF',
+            'github-slateGray': '#697C9A',
+            'github-queenBlue': '#4B6A9B',
+            'github-gunMetal': '#2B3442',
+            'github-ghostWhite': '#F6F8FF',
+            'github-white': '#FFFFFF',
+            'github-darkGunMetal': '#141D2F',
+            'github-yankeesBlue': '#1E2A47',
+        },
     },
     plugins: [],
 };
