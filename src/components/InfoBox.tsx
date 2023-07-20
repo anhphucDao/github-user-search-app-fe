6c939cc682be37e9de04050120b47e35c0e9c54b
Andrew Dao <daoanhphuc84@gmail.com>
2023-07-20 20:12:35 +0700

    [f] feature: infoBox - Initialize draft layout for info box container

---
+++ src/components/InfoBox.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { ReactComponent as LocationIcon } from '../assets/icon-location.svg';
+// import { ReactComponent as TwitterIcon } from '../assets/icon-twitter.svg';
+// import { ReactComponent as WebsiteIcon } from '../assets/icon-website.svg';
+// import { ReactComponent as CompanyIcon } from '../assets/icon-company.svg';
+
+export default function InfoBox() {
+    return (
+        <div className="flex flex-col w-[20.44rem] h-[32.31rem] sm:w-[35.81rem] sm:h-[30.06rem] md:w-[45.63rem] md:h-[27.75rem] rounded-[0.94rem] drop-shadow-lg bg-github-white mt-[1rem] sm:mt-[1.5rem] mb-[5rem] sm:mb-[14.75rem] md:mb-[9rem]"></div>
+    );
+}
