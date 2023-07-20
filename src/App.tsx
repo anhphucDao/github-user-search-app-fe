0638b73d3e046fc8bc62797a1743190ad0faa778
Andrew Dao <daoanhphuc84@gmail.com>
2023-07-20 15:45:25 +0700

    [f] feature: header -finish basic light theme header

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
 import './index.css';
+import Header from '../src/components/Header';
 
 function App() {
     return (
-        <>
-            <div className="font-space">
-                Adipisicing eiusmod exercitation aliqua do ea id proident nostrud amet id
-                adipisicing. Sint est deserunt enim ex minim ad minim ex incididunt ex aliqua velit
-                sunt. Commodo laboris exercitation culpa dolor amet excepteur elit est sunt nulla
-                occaecat. Ad Lorem proident irure sint ad irure commodo voluptate in anim do.
-                Laborum dolore aliqua eu deserunt culpa labore ullamco sunt deserunt. Nulla enim
-                consectetur irure officia labore ipsum voluptate non consectetur. Nostrud nisi
-                cupidatat aliquip anim fugiat esse eu nulla deserunt consectetur.
-            </div>
-
-            <div className="font-space font-bold bg-github-gunMetal text-github-white text-[1.5rem]">
-                Ad cupidatat consectetur ullamco consectetur Lorem fugiat do sunt enim culpa minim
-                velit mollit. Laborum officia in excepteur aute nulla duis reprehenderit cupid
-                officia aliqua quis deserunt culpa. Anim consequat non cupidatat reprehenderit
-                aliqua. Labore id aute cupidatat incididunt. Reprehenderit proident quis eiusmod
-                incididunt cillum ex do labore ullamco occaecat aliquip nisi tempor nulla. Mollit
-                quis aliquip voluptate est non laborum Lorem magna sit adipisicing in ut.
-            </div>
-        </>
+        <div>
+            <Header />
+        </div>
     );
 }
 
