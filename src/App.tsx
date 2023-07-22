8bf9ede00e22f05a88e123ca0de8f2b207c03770
Andrew Dao <daoanhphuc84@gmail.com>
2023-07-22 18:43:49 +0700

    [f] feature: get-user-data - finish layout with userData - Handle no result cases

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,15 @@ import DataProvider from '../src/context/DataContext';
 
 function App() {
     return (
-        <div className="flex flex-col mx-auto mt-[1.94rem] sm:mt-[8.75rem] md:mt-[9rem] items-center font-space h-[41.6875rem]">
-            <DataProvider>
-                <Header />
-                <SearchBar />
-                <InfoBox />
-            </DataProvider>
-        </div>
+        <DataProvider>
+            <div className="min-h-screen min-w-full bg-github-ghostWhite flex-1 flex">
+                <div className="flex flex-col mx-auto my-auto mt-[1.94rem] sm:mt-[8.75rem] md:mt-[9rem] items-center font-space h-[41.6875rem] max-h-[41.6875rem] mb-[5rem]">
+                    <Header />
+                    <SearchBar />
+                    <InfoBox />
+                </div>
+            </div>
+        </DataProvider>
     );
 }
 
